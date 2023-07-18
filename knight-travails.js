8b805fcfeab8e324527cfbb058c5911787eb5b61
David Forrester <david.o.forrester@gmail.com>
2023-07-18 20:10:53 +1000

    commit

--- knight-travails.js
+++ knight-travails.js
@@ -1,76 +1,76 @@
-class knight {
-  constructor() {
-    this.x = 0;
-    this.y = 0;
-    this.moves = [];
+function getAvalMoves(location) {
+  let nextMoveArray = [];
+  let horr = location[0];
+  let vert = location[1];
+
+  for (let i = 0; i < 8; i++) {
+    switch (i) {
+      case 0:
+        horr += 2;
+        vert += 1;
+        break;
+      case 1:
+        horr += 1;
+        vert += 2;
+        break;
+      case 2:
+        horr -= 2;
+        vert -= 1;
+        break;
+      case 3:
+        horr -= 1;
+        vert -= 2;
+        break;
+      case 4:
+        horr += 1;
+        vert -= 2;
+        break;
+      case 5:
+        horr -= 1;
+        vert += 2;
+        break;
+      case 6:
+        horr += 2;
+        vert -= 1;
+        break;
+      case 7:
+        horr -= 2;
+        vert += 1;
+        break;
+      default:
+        break;
+    }
+
+    if (horr > 7 || horr < 0 || vert > 7 || vert < 0) {
+    } else {
+      nextMoveArray.push([horr, vert]);
+    }
+    horr = location[0];
+    vert = location[1];
+  }
+  return nextMoveArray;
+}
+
+class Knight {
+  constructor(data) {
+    this.data = data;
+  }
+}
+
+class Board {
+  constructor(arr) {
+    this.start = arr;
   }
 
-  getAvalMoves() {
-    let nextMoveArray = [];
-    let horr = this.x;
-    let vert = this.y;
-
-    for (let i = 0; i < 8; i++) {
-      switch (i) {
-        case 0:
-          horr += 2;
-          vert += 1;
-          break;
-        case 1:
-          horr += 1;
-          vert += 2;
-          break;
-        case 2:
-          horr -= 2;
-          vert -= 1;
-          break;
-        case 3:
-          horr -= 1;
-          vert -= 2;
-          break;
-        case 4:
-          horr += 1;
-          vert -= 2;
-          break;
-        case 5:
-          horr -= 1;
-          vert += 2;
-          break;
-        case 6:
-          horr += 2;
-          vert -= 1;
-          break;
-        case 7:
-          horr -= 2;
-          vert += 1;
-          break;
-        default:
-          break;
-      }
-
-      if (horr > 7 || horr < 0 || vert > 7 || vert < 0) {
-      } else {
-        nextMoveArray.push([horr, vert]);
-      }
-      horr = this.x;
-      vert = this.y;
-    }
-    return nextMoveArray;
+  buildMoves(arr) {
+    console.log(getAvalMoves(arr));
   }
 }
 
 function knightMoves(start, end) {
-  const newKnight = new knight();
+  let board = new Board(start);
 
-  newKnight.x = start[0];
-  newKnight.y = start[1];
-
-  console.log(newKnight.getAvalMoves());
-
-  //console.log(newKnight.moves);
+  board.buildMoves(start);
 }
 
-const start = [1, 0];
-const end = [7, 7];
-
-const moves = knightMoves(start, end);
+knightMoves([0, 0], [3, 3]);
